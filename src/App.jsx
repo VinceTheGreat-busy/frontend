29ca9d9c01f2bf1bdff43a1dbdae9d661ecc16ec
Vince <vincepalaming2108@gmail.com>
2026-08-19 22:12:57 +0800

    fixed App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import Layout from "./Components/Layout";
 import Home from "./Pages/Home";
 
 function Pages() {
   return (
     <Routes>
-      <Route index="/" element={<Layout /> }>
-        <Route element={<Home />} />
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home />} />
       </Route>
     </Routes>
-  )
+  );
 }
 
 export default function App() {
