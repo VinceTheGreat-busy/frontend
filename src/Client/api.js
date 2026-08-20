c313518fd1f1b032eea849f7a5d27a79d4389ea0
Vince <vincepalaming2108@gmail.com>
2026-08-20 14:43:38 +0800

    api

--- src/Client/api.js
+++ src/Client/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 
 const api = axios.create({
-    baseURL: import.meta.env.BACKEND_URL,
+    baseURL: "https://backend-1-lwj5.onrender.com/api",
 
     withCredentials: true,
     withXSRFToken: true,
