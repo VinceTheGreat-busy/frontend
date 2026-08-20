8fadd60a7bdc52fa38ade8989565456e9e131097
Vince <vincepalaming2108@gmail.com>
2026-08-20 15:41:22 +0800

    try login

--- src/Client/api.js
+++ src/Client/api.js
@@ -1,12 +1,9 @@
 import axios from "axios";
 
-
 const api = axios.create({
     baseURL: "https://backend-1-lwj5.onrender.com",
-
     withCredentials: true,
     withXSRFToken: true,
-
     headers: {
         Accept: "application/json",
         "Content-Type": "application/json",
