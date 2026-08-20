131cb9b120c5ac4ec79e4b0c98938427d6e4a087
Vince <vincepalaming2108@gmail.com>
2026-08-20 14:53:42 +0800

    API

--- src/Client/api.js
+++ src/Client/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 
 const api = axios.create({
-    baseURL: "https://backend-1-lwj5.onrender.com/api",
+    baseURL: "https://backend-1-lwj5.onrender.com",
 
     withCredentials: true,
     withXSRFToken: true,
